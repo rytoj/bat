0d937dc447a02510e101c036a10e408ffec1f930
rytoj <atobiren@gmail.com>
2015-10-26 16:42:52 +0200

    Create oneliners.bat

---
+++ oneliners.bat
@@ -0,0 +1,15 @@
+#shutdown remote pc
+shutdown -m \\server -r -t 0
+
+#find IP NETBIOS name
+nbtstat -a 192.168.1.50
+
+#Quickly get my MAC
+cmd /k ipconfig /all
+
+#Windows version
+winver
+
+#opens explorer
+explorer .
+start .
